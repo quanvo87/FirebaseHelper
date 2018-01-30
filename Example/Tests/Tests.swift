95ff199dfc2080b5ddf8b47813c51b86a2cd5818
Quan Vo <quan.vo@ibm.com>
2018-01-29 23:29:22 -0600

    update readme (#1)
    BREAKING CHANGE: Add external param name to increment for clarity. Update README.

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,29 +1,138 @@
-import UIKit
-import XCTest
-import FirebaseHelper
+//import XCTest
+//import FirebaseHelper
+//
+//class Tests: XCTestCase {
+//    func testDecrement() {
+//        let expectation = self.expectation(description: #function)
+//        defer { waitForExpectations(timeout: 10) }
+//
+//        let rand1 = Int(arc4random_uniform(500))
+//        let rand2 = Int(arc4random_uniform(500))
+//        FirebaseHelper.main.set(rand1, at: "test") { (error) in
+//            XCTAssertNil(error, String(describing: error))
+//            let work = GroupWork()
+//            for _ in 1...rand2 {
+//                work.start()
+//                FirebaseHelper.main.increment(-1, at: "test") { (error) in
+//                    XCTAssertNil(error, String(describing: error))
+//                    work.finish(withResult: true)
+//                }
+//            }
+//            work.allDone {
+//                FirebaseHelper.main.get("test") { (result) in
+//                    switch result {
+//                    case .failure(let error):
+//                        XCTFail(String(describing: error))
+//                        expectation.fulfill()
+//                    case .success(let data):
+//                        XCTAssertEqual(data.value as? Int, rand1 - rand2)
+//                        expectation.fulfill()
+//                    }
+//                }
+//            }
+//        }
+//    }
+//
+//    func testGetSetDelete() {
+//        let expectation = self.expectation(description: #function)
+//        defer { waitForExpectations(timeout: 10) }
+//
+//        FirebaseHelper.main.set("a", at: "test") { (error) in
+//            XCTAssertNil(error, String(describing: error))
+//
+//            FirebaseHelper.main.get("test") { (result) in
+//                switch result {
+//                case .failure(let error):
+//                    XCTFail(String(describing: error))
+//                    expectation.fulfill()
+//                case .success(let data):
+//                    XCTAssertEqual(data.value as? String, "a")
+//
+//                    FirebaseHelper.main.delete("test") { (error) in
+//                        XCTAssertNil(error, String(describing: error))
+//
+//                        FirebaseHelper.main.get("test") { (result) in
+//                            switch result {
+//                            case .failure(let error):
+//                                XCTFail(String(describing: error))
+//                                expectation.fulfill()
+//                            case .success(let data):
+//                                XCTAssertFalse(data.exists())
+//                                expectation.fulfill()
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+//
+//    func testIncrement() {
+//        let expectation = self.expectation(description: #function)
+//        defer { waitForExpectations(timeout: 10) }
+//
+//        FirebaseHelper.main.increment(1, at: "test") { (error) in
+//            XCTAssertNil(error, String(describing: error))
+//
+//            FirebaseHelper.main.get("test") { (result) in
+//                switch result {
+//                case .failure(let error):
+//                    XCTFail(String(describing: error))
+//                    expectation.fulfill()
+//                case .success(let data):
+//                    XCTAssertEqual(data.value as? Int, 1)
+//                    expectation.fulfill()
+//                }
+//            }
+//        }
+//    }
+//
+//    func testIncrementConcurrent() {
+//        let expectation = self.expectation(description: #function)
+//        defer { waitForExpectations(timeout: 10) }
+//
+//        let incrementsDone = DispatchGroup()
+//        for _ in 1...2 {
+//            incrementsDone.enter()
+//            FirebaseHelper.main.increment(1, at: "test") { (error) in
+//                XCTAssertNil(error, String(describing: error))
+//                incrementsDone.leave()
+//            }
+//        }
+//        incrementsDone.notify(queue: .main) {
+//            FirebaseHelper.main.get("test") { (result) in
+//                switch result {
+//                case .failure(let error):
+//                    XCTFail(String(describing: error))
+//                    expectation.fulfill()
+//                case .success(let data):
+//                    XCTAssertEqual(data.value as? Int, 2)
+//                    expectation.fulfill()
+//                }
+//            }
+//        }
+//    }
+//
+//    func testMakeDatabaseReference() {
+//        do {
+//            _ = try FirebaseHelper.main.makeReference("a")
+//            _ = try FirebaseHelper.main.makeReference("a", "b")
+//            _ = try FirebaseHelper.main.makeReference("/a/")
+//            _ = try FirebaseHelper.main.makeReference("//a//")
+//            _ = try FirebaseHelper.main.makeReference("/a/a/")
+//        } catch {
+//            XCTFail()
+//        }
+//    }
+//
+//    func testMakeDatabaseReferenceFail() {
+//        XCTAssertThrowsError(_ = try FirebaseHelper.main.makeReference(""))
+//        XCTAssertThrowsError(_ = try FirebaseHelper.main.makeReference("a", ""))
+//        XCTAssertThrowsError(_ = try FirebaseHelper.main.makeReference("", "a"))
+//        XCTAssertThrowsError(_ = try FirebaseHelper.main.makeReference("/"))
+//        XCTAssertThrowsError(_ = try FirebaseHelper.main.makeReference("//"))
+//        XCTAssertThrowsError(_ = try FirebaseHelper.main.makeReference("///"))
+//        XCTAssertThrowsError(_ = try FirebaseHelper.main.makeReference("/a//a/"))
+//    }
+//}
 
-class Tests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
-}
--- README.md
+++ README.md
@@ -5,11 +5,18 @@
 [![License](https://img.shields.io/cocoapods/l/FirebaseHelper.svg?style=flat)](http://cocoapods.org/pods/FirebaseHelper)
 [![Platform](https://img.shields.io/cocoapods/p/FirebaseHelper.svg?style=flat)](http://cocoapods.org/pods/FirebaseHelper)
 
-## Example
+FirebaseHelper is a small wrapper over Firebase's realtime database, providing streamlined methods for get, set, delete, and increment values.
 
-To run the example project, clone the repo, and run `pod install` from the Example directory first.
+## Features
+- [x] Setup Firebase Realtime Database Ref
+- [x] Read values (get)
+- [x] Write/Update values (set)
+- [x] Remove values (delete)
+- [x] Increment values (increment)
 
 ## Requirements
+
+Swift 4
 
 ## Installation
 
@@ -20,9 +27,129 @@ it, simply add the following line to your Podfile:
 pod 'FirebaseHelper'
 ```
 
-## Author
+## Usage
 
-quanvo87, qvo1987@gmail.com
+Initialize an instance of `FirebaseHelper`:
+
+```swift
+import Firebase
+import FirebaseHelper
+
+let firebaseHelper = FirebaseHelper(FirebaseDatabase.Database.database().reference())
+```
+
+`FirebaseHelper(_ ref: DatabaseReference)` takes in a `DatabaseReference`. Generally you'd want this to be the root of your database.
+
+For convenience, you can add something like this to your project:
+
+```swift
+extension FirebaseHelper {
+    static let main: FirebaseHelper = {
+        FirebaseHelper(FirebaseDatabase.Database.database().reference())
+    }()
+}
+```
+
+And now you can simply call `FirebaseHelper.main` to access this instance of `FirebaseHelper` from anywhere in your project.
+
+### Common Database Operations
+
+#### Get
+
+Example:
+
+```swift
+FirebaseHelper.main.get("users", "john123", "favoriteFood") { result in
+    switch result {
+      case .failure(let error):
+        // handle error
+      case .success(let data):
+        // get string from data
+    }
+}
+```
+
+API:
+
+```swift
+public func get(_ first: String, _ rest: String..., completion: @escaping (Result<DataSnapshot, Error>) -> Void)
+```
+
+`get()` takes in a variadic parameter of child nodes that will be built on the `DatabaseReference` you initialized your instance of `FirebaseHelper` on.
+
+The callback returns a `Result`:
+
+```swift
+public enum Result<T, Error> {
+    case success(T)
+    case failure(Error)
+}
+```
+
+In this case, `T` is `DataSnapshot`. An error case will either be because an invalid child was passed in or some other error in your database.
+
+#### Set, Delete, Increment
+
+`set()`, `delete()`, and `increment()` work similarly, but instead of returning a `Result`, they simply return an `Error` if one occurred, or `nil` otherwise.
+
+Examples:
+
+```swift
+// The first parameter is an `Any` that gets set at the specified path.
+FirebaseHelper.main.set("pizza", at: "users", "john123", "favoriteFood") { error in
+    if let error = error {
+      // handle error
+  }
+}
+
+FirebaseHelper.main.delete("users", "john123", "favoriteFood") { error in
+    if let error = error {
+      // handle error
+  }
+}
+
+FirebaseHelper.main.increment(by: 5, "users", "john123", "favoriteFoodEatenThisMonth") {
+    if let error = error {
+      // handle error
+  }
+}
+```
+
+APIs:
+
+```swift
+public func set(_ value: Any, at first: String, _ rest: String..., completion: @escaping (Error?) -> Void)
+
+public func delete(_ first: String, _ rest: String..., completion: @escaping (Error?) -> Void)
+
+public func increment(by amount: Int, at first: String, _ rest: String..., completion: @escaping (Error?) -> Void)
+```
+
+> Note: You should only `set()` accepted value types. See [Firebase docs](https://firebase.google.com/docs/database/ios/read-and-write#basic_write).
+
+#### Safely Make A DatabaseReference
+
+You will often need to call more complex `FirebaseDatabase` functions, such as building a query and calling `observe(_ eventType: with block:)` on it. `FirebaseHelper` can still help with that:
+
+```swift
+let ref = try? FirebaseHelper.main.makeReference("users", "john123", "eatingHistory")
+let handle = ref?.queryOrdered(byChild: "timestamp").queryLimited(toLast: 50).observe(.value) { data in
+    // handle data
+}
+```
+
+API:
+
+```swift
+public func makeReference(_ first: String, _ rest: String...) throws -> DatabaseReference
+```
+
+`makeReference` will throw an error if passed an invalid child.
+
+## Collaborators
+
+- [Quan Vo](https://github.com/quanvo87)
+- [Wilson Ding](https://github.com/dingwilson)
 
 ## License
 
